3cde5d7c7bb2e263cd09ec41b0d67ade2bacd5da
awaiszul <141385734+awaiszul@users.noreply.github.com>
2023-08-04 08:46:44 +0500

    Add files via upload

---
+++ full.js
@@ -0,0 +1,25 @@
+function toggleHide() {
+    let btn= document.getElementById("btn");
+    let para= document.getElementById("ipara");
+    // para.style.display="block";
+    if  (para.style.display!='none') {
+    
+        para.style.display='none';
+    }
+    else{
+        para.style.display="block";
+         
+    }
+}
+function togglehide() {
+    let href=document.getElementById("ca");
+    let ags=document.getElementById("apara");
+    if  (para.style.display!='none') {
+    
+        para.style.display='block';
+    }
+    else{
+        para.style.display="none";
+         
+    }
+}
